f6300aa66677896928f1a64d8ac38532fc186aee
ngee044 <38107616+ngee044@users.noreply.github.com>
2025-03-10 21:55:43 +0900

    버전에 따른 컴파일 에러 수정

--- Network/NetworkClient.cpp
+++ Network/NetworkClient.cpp
@@ -247,8 +247,11 @@ namespace Network
 		{
 			current_socket->open(boost::asio::ip::tcp::v4());
 			current_socket->bind(boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 0));
+#if BOOST_VERSION >= 106600
+			current_socket->connect(boost::asio::ip::tcp::endpoint(boost::asio::ip::make_address(ip), port));
+#else
 			current_socket->connect(boost::asio::ip::tcp::endpoint(boost::asio::ip::address::from_string(ip), port));
-
+#endif	
 			current_socket->set_option(boost::asio::ip::tcp::no_delay(true));
 			current_socket->set_option(boost::asio::socket_base::keep_alive(true));
 			current_socket->set_option(boost::asio::socket_base::receive_buffer_size(buffer_size()));
