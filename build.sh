355d1ed95e084e669cdc92c1af63ecfde9024b4e
ngee044 <38107616+ngee044@users.noreply.github.com>
2026-02-27 21:52:23 +0900

    사용 컴파일러 23으로 변경

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ rm -rf build
 
 mkdir build
 cd build
-cmake .. -G Ninja -DCMAKE_TOOLCHAIN_FILE="../../vcpkg/scripts/buildsystems/vcpkg.cmake" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF
+cmake .. -G Ninja -DCMAKE_TOOLCHAIN_FILE="../../vcpkg/scripts/buildsystems/vcpkg.cmake" -DVCPKG_OVERLAY_TRIPLETS="../custom-triplets" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF
 ninja
 export LC_ALL=C
